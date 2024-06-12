cb7e4a4c041404ae44318c525679f0fbdb757b4a
Nicolas <naguirre@eagerworks.com>
2024-06-12 11:01:26 -0300

    feat: filter products added to homepage, fix: refactor routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,26 @@ class HomeController < ApplicationController
     @products = Product.page(params[:page]).per(10)
   end
 
+  def filter
+    @products = Product.all
+
+    filter_params.each do |key, value|
+      @products = @products.where(key => true) if value == '1'
+    end
+
+    @products = @products.page(params[:page]).per(10)
+    render :index
+  end
+
   private
+
   def authenticate_client!
     unless client_signed_in?
       redirect_to new_client_session_path, alert: "You need to sign in as a client to access this page."
     end
+  end
+
+  def filter_params
+    params.permit(:for_sharing, :vegan_or_vegetarian, :sugar_free, :no_tacc, :apetizer, :commit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,29 +9,30 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  Rails.application.routes.draw do
   get 'home/index'
-    devise_for :admins, controllers: {
-      sessions: 'admins/sessions',
-      registrations: 'admins/registrations',
-      passwords: 'admins/passwords'
-    }
 
-    devise_for :clients, controllers: {
-      sessions: 'clients/sessions',
-      registrations: 'clients/registrations',
-      passwords: 'clients/passwords'
-    }
+  get 'filter', to: 'home#filter', as: 'filter_products'
 
-    devise_for :suppliers, controllers: {
-      sessions: 'suppliers/sessions',
-      registrations: 'suppliers/registrations',
-      passwords: 'suppliers/passwords'
-    }
-  end
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations',
+    passwords: 'admins/passwords'
+  }
+
+  devise_for :clients, controllers: {
+    sessions: 'clients/sessions',
+    registrations: 'clients/registrations',
+    passwords: 'clients/passwords'
+  }
+
+  devise_for :suppliers, controllers: {
+    sessions: 'suppliers/sessions',
+    registrations: 'suppliers/registrations',
+    passwords: 'suppliers/passwords'
+  }
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  # Products
   resources :products, only: [:index, :show]
 end
