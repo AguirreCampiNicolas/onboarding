f7ae60d987ea5cbefff85d341b107135832cf123
Nicolas <naguirre@eagerworks.com>
2024-06-13 13:37:49 -0300

    fix: fix javascript loading bug, added styles and navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import 'flowbite';
-import 'flowbite-datepicker';
-import 'flowbite/dist/datepicker.turbo.js';
+import 'flowbite'
+import 'flowbite/dist/datepicker.turbo.js'
+import "./controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
 pin "flowbite-datepicker", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.turbo.min.js"
 pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
